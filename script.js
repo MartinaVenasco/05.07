30a3d575a9b98f9229fb2cc56ca51e04dd98fad8
MartinaVenasco <martinavenasco@gmail.com>
2022-07-06 16:52:44 +0200

    chore: exercise complited

--- script.js
+++ script.js
@@ -14,99 +14,76 @@ const totalPrice = addonsPrince.reduce((a, b) => a + b);
 console.log(totalPrice + product.price);
 console.clear();
 
-const getProductHTML = (product) => {
-  const { name, price } = product;
-  return `<li class="item">${name} - ${price}€</li>`;
-};
+const newProducts = Array.from(
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  (_, index) => {
+    return {
+      id: 1,
+      name: `TV - ${_}`,
+      price: 40 + index,
+    };
+  }
+);
 
 const shop = {
-  name: "Edgemonics",
-  _products: [],
-  _page: 0, // pagina corrente
-  _per_page: 2, // numero di risultati per pagina
+  products: [],
+  page: 0,
+  perPage: 5,
 
-  get products() {
-    /**
-     * Qui dentro dovremmo riuscire a paginare i prodotti.
-     * Possiamo procurarci un indice iniziale ed uno finale lavorando con this._page e this._per_page
-     * */
-    console.log("Stai leggendo i prodotti di ", this.name);
-    const startIndex = 0; // ...
-    const endIndex = 6; // ...
-    const paginatedProducts = this._products
-
-    return paginatedProducts;
+  setPage(newPage) {
+    this.page = newPage;
+    this.renderHTML();
   },
 
-  set products(newProducts) {
-    /**
-     * Il consiglio è quello di spostare la parte di renderHTML dentro una funzione indipendente,
-     * così da rendere il metodo più snello
-     * **/
+  getProducts() {
+    const startIndex = this.page * this.perPage;
+    const endIndex = startIndex + this.perPage;
 
-    this._products = newProducts;
+    const slicedProducts = this.products.slice(startIndex, endIndex);
 
-    const productsHTML = this.products.map(getProductHTML).join("");
-    document.body.innerHTML = `
-		  <h2>Offerte lampo</h2>
-		  <div class="container"><ul>${productsHTML}</ul></div>
-		<div class="button">
-		  <button id="1" class="button-one">1</button>
-		  <button id="2" class="button-two">2</button>
-		  <button id="3" class="button-three">3</button></div>
-	 
-	  `;
-	  
+    return slicedProducts;
   },
 
-
-
-  set page(newPage) {
-    /**
-     * Aggiorniamo la pagina corrente.
-     * Sarà che dobbiamo aggiornare il DOM anche qui?
-     * */
+  setProducts(products) {
+    this.products = products;
+    this.renderHTML();
   },
 
   renderHTML() {
-    /**
-     * Aggiorniamo il DOM stampando i risultati a schermo.
-     * Avendo ora anche la paginazione, sarebbe il caso di mettere il nostro shop dentro un div specifico div.shop
-     * con una struttura del genere
-     * <body>
-     *  div.shop -> aggiornato ad ogni chiamata della funzione
-     *  div.pagination -> questo non si tocca mai
-     * </body
-     * e gestire la paginazione in modo separato, inserendo gli event listener una sola volta
-     * **/
+    const productsHTML = this.getProducts()
+      .map((product) => {
+        const { name, price } = product;
+        //
+        return `<li>${name} - ${price}€</li>`;
+      })
+      .join("");
+    document.querySelector(".shop").innerHTML = `
+    <h2>PRODOTTI DISPONIBILI SUBITO!</h2>
+    
+    <ul>${productsHTML}</ul><div class="countp" > Page: ${this.page}</div>
+    <div class="countp" >Showing: ${this.getProducts().length}/${this.products.length}</div>
+`;
   },
 };
-shop.products = [
-  {
-    name: "tv",
-    price: "1345",
-  },
-  {
-    name: "pc",
-    price: "875",
-  },
-  {
-    name: "smartphone",
-    price: 748,
-  },
-  {
-    name: "lavatrice",
-    price: "236",
-  },
-  {
-    name: "aspirapolvere",
-    price: "354",
-  },
 
-  {
-    name: "mouse",
-    price: "14",
-  },
-];
+shop.setProducts(newProducts);
 
-const button1 = document.querySelector('button[1]');
+const $buttons = document.querySelectorAll(".pagination button");
+
+$buttons.forEach(($button, index, array) => {
+  console.log($button);
+
+  $button.id = index;
+
+  $button.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    event.stopPropagation();
+
+    const newPage = Number(this.innerText) - 1;
+
+    shop.setPage(newPage);
+
+    console.log("shop.page", shop.page);
+  });
+});
